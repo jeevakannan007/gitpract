dc58381278ee5e5591ca3ecf2060d39c2719998a
Jeeva Kannan <jeevakannan@example.com>
2025-12-16 16:22:36 +0530

    Initial commit

---
+++ src/main/java/com/example/cicd_fie/CicdFieApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_fie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdFieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdFieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicd_fie/CicdFieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_fie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdFieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
